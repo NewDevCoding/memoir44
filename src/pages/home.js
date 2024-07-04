b4f002acbb8f538474e903612b954555cc2532c9
Isaak <isaac@thesheridans.email>
2024-07-04 14:46:34 -0400

    commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import "./home.css"
+
 
 const home = () => {
   return (
-    <img src='Textphoto.png' />
-      
+    <>
+      <div className='container'>
+        <div className='test'>
+          <h2 className='main-text'>
+            Memoir44.com is built by fans, for fans. The information here is meant <br></br>
+            to make your gaming experience more enjoyable and convienent. Here you can find
+            all the rules for terrain, troops, markers, and special unit and army rules all
+            in one easy place. On top of that, you now have access to detailed digital setup
+            instructions for every offcial Memoir44 scenario produced as well as many fan-made 
+            ones. 
+          </h2>;
+        </div>            
+      </div>    
+    
+      <footer>
+        <div className='footer-1'></div>
+
+      </footer>
+    </>
+    
     
   )
 }
