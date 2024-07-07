e1cb6c8e6e73ff75cd0a7080606cbcde6bb8b6a5
Isaak <isaac@thesheridans.email>
2024-07-07 14:54:51 -0400

    created test search bar for troops

--- src/pages/abacus.js
+++ src/pages/abacus.js
@@ -7,30 +7,3 @@ const Abacus = () => {
   )
 }
 export default Abacus
-
-// const home = () => {
-//   return (
-//     <>
-//       <div className='container'>
-//         <div className='test'>
-//           <h2 className='main-text'>
-//             Memoir44.com is built for fans, by fans. The information here is meant <br></br>
-//             to make your gaming experience more enjoyable and convienent. Here you can find
-//             all the rules for terrain, troops, markers, and special unit and army rules all
-//             in one easy place. On top of that, you now have access to detailed digital setup
-//             instructions for every offcial Memoir44 scenario produced as well as many fan-made 
-//             ones. 
-//           </h2>;
-//         </div>            
-//       </div>    
-    
-//       <footer className='footer-1'>
-//         <div className='contact'>Contact me: isaac@thesheridans.email</div>
-//         <div className='copywrite'>Copywrite &copy;2024;  Designed by Molotovboi</div>
-
-//       </footer>
-//     </>
-    
-    
-//   )
-// }
--- src/pages/troops.js
+++ src/pages/troops.js
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
+import CustomerData from "./customerData"
+import Card from './card'
+import "./troops.css"
 
-const Troops = () => {
-  return (
-    <>
-     <div className='search-wrapper'>
-       <label htmlFor='search'>Search for Troops</label>
-       <input type='search' placeholder='Search for troops' id='search'/>
-     </div>
 
-     <div className='troop-cards'>
-      
-     </div>
-    </>
-    
-    
-  )
-}
+
+
+function Troops() {
+    const [tempData, setTempData] = useState(CustomerData);
+    const inputStyle = {
+      padding: 12,
+      width: "100%",
+      fontSize: "105%",
+    };
+    const onSearchChange = (value) => {
+      const newData = CustomerData && CustomerData.filter(
+        (cust) =>
+          cust.name.toLocaleLowerCase().includes(value.toLocaleLowerCase()) ||
+          cust.order.includes(value)
+      );
+      setTempData(newData);
+    };
+    return (
+      <div className="container">
+        <h2>React-App</h2>
+        <h4>Build Search Filter in React Js</h4>
+        <input
+          type="search"
+          placeholder="Type something to search..."
+          style={inputStyle}
+          onChange={(e) => onSearchChange(e.target.value)}
+        />
+        <div className='display-container'>
+        {tempData && tempData.map((customer, index) => (
+          <Card userInfo={customer} key={index} />
+        ))}
+        </div>
+      </div>
+    );
+  }
+
+
 
 export default Troops
-
