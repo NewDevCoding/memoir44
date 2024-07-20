fb7fd81b390338b92d53f2da3a95719689f7e0cc
Isaak <isaac@thesheridans.email>
2024-07-20 09:40:28 -0400

    added other troop cards

--- src/pages/troop_dict.js
+++ src/pages/troop_dict.js
@@ -1,40 +1,124 @@
-const Infantry = () => {
-    return (
-      <> 
-            
-      </>
-    )
-  }
+const Test18 = () => {
+  return (
+    <div>Test18</div>
+          
+    
+  )
+}
+const Test17 = () => {
+  return (
+    <div>Test17</div>
+          
+    
+  )
+}
+const Test16 = () => {
+  return (
+    <div>Test16</div>
+          
+    
+  )
+}
+const Test15 = () => {
+  return (
+    <div>Test15</div>
+          
+    
+  )
+}
+const Test14 = () => {
+  return (
+    <div>Test14</div>
+          
+    
+  )
+}
+const Test13 = () => {
+  return (
+    <div>Test13</div>
+          
+    
+  )
+}
+const Test12 = () => {
+  return (
+    <div>Test12</div>
+          
+    
+  )
+}
+const Test11 = () => {
+  return (
+    <div>Test11</div>
+          
+    
+  )
+}
+const Test10 = () => {
+  return (
+    <div>Test10</div>
+          
+    
+  )
+}
+const Test9 = () => {
+  return (
+    <div>Test9</div>
+          
+    
+  )
+}
+const Test8 = () => {
+  return (
+    <div>Test8</div>
+          
+    
+  )
+}
+const Test7 = () => {
+  return (
+    <div>Test7</div>
+          
+    
+  )
+}
+
+const Test6 = () => {
+  return (
+    <div>Test6</div>
+  )
+}
+
   
-  const Test5 = () => {
+const Test5 = () => {
     return (
       <div>Test5</div>
     )
   }
   
   
-  const Test4 = () => {
+const Test4 = () => {
     return (
       <div>Test4</div>
     )
   }
   
   
-  const Test3 = () => {
+const Test3 = () => {
     return (
       <div>Test3</div>
     )
   }
   
   
-  const Test2 = () => {
+const Test2 = () => {
     return (
       <div>Test2</div>
     )
   }
   
   
-  const Test1 = () => {
+const Test1 = () => {
     return (
       <div>Test1</div>
     )
@@ -42,7 +126,7 @@ const Infantry = () => {
   
   let dict = [{
     id: 1,
-    component: <Infantry />
+    component: <Test1 />
   },{
     id: 2,
     component: <Test2 />
@@ -57,7 +141,43 @@ const Infantry = () => {
     component: <Test5 />
   },{
     id: 6,
-    component: <Infantry />
+    component: <Test6 />
+  },{
+    id: 7,
+    component: <Test7 />
+  },{
+    id: 8,
+    component: <Test8 />
+  },{
+    id: 9,
+    component: <Test9 />
+  },{
+    id: 10,
+    component: <Test10 />
+  },{
+    id: 11,
+    component: <Test11 />
+  },{
+    id: 12,
+    component: <Test12 />
+  },{
+    id: 13,
+    component: <Test13 />
+  },{
+    id: 14,
+    component: <Test14 />
+  },{
+    id: 15,
+    component: <Test15 />
+  },{
+    id: 16,
+    component: <Test16 />
+  },{
+    id: 17,
+    component: <Test17 />
+  },{
+    id: 18,
+    component: <Test18 />
   }]
 
   export default dict
--- src/pages/troops.js
+++ src/pages/troops.js
@@ -10,12 +10,72 @@ const CustomerData = [{
   image: require("./troop_images/mobile_artillery.PNG"),
 },{
   id: 2,
-  name: 'Artificial Intelligence',
-  image: require("./troop_images/mobile_artillery.PNG"),
+  name: 'Armoured Trains',
+  image: require("./troop_images/armoured_trains.PNG"),
 },{
   id: 3,
-  name: 'Intelligence',
-  image: require("./troop_images/mobile_artillery.PNG"),
+  name: 'Big Guns',
+  image: require("./troop_images/big_guns.PNG"),
+},{
+  id: 4,
+  name: 'Brandenburgers',
+  image: require("./troop_images/branded_burgers.PNG"),
+},{
+  id: 5,
+  name: 'Combat Engineers',
+  image: require("./troop_images/combat_engineers.PNG"),
+},{
+  id: 6,
+  name: 'Command Cars',
+  image: require("./troop_images/command_Cars.PNG"),
+},{
+  id: 7,
+  name: 'Depleted Units',
+  image: require("./troop_images/depleted_units.PNG"),
+},{
+  id: 8,
+  name: 'Destroyers',
+  image: require("./troop_images/destroyers.PNG"),
+},{
+  id: 9,
+  name: 'Half Tracks',
+  image: require("./troop_images/half_tracks.PNG"),
+},{
+  id: 10,
+  name: 'Heavy Anti-Tank',
+  image: require("./troop_images/heavy_at.PNG"),
+},{
+  id: 11,
+  name: 'Hobarts Funnies',
+  image: require("./troop_images/hobarts.PNG"),
+},{
+  id: 12,
+  name: 'Screeming Meemies',
+  image: require("./troop_images/screeming_meemies.PNG"),
+},{
+  id: 13,
+  name: 'Ski Troops',
+  image: require("./troop_images/ski_troops.PNG"),
+},{
+  id: 14,
+  name: 'Snipers',
+  image: require("./troop_images/snipers.PNG"),
+},{
+  id: 15,
+  name: 'Supply Trains',
+  image: require("./troop_images/supply_train.PNG"),
+},{
+  id: 16,
+  name: 'Supply Trucks',
+  image: require("./troop_images/supply_trucks.PNG"),
+},{
+  id: 17,
+  name: 'Tank Destroyers',
+  image: require("./troop_images/tank_destroyers.PNG"),
+},{
+  id: 18,
+  name: 'Tigers',
+  image: require("./troop_images/tigers.PNG"),
 }]
 
  
