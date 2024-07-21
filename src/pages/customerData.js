44bba89fc9528a68654eeabc43a2571407c90c5c
Isaak <isaac@thesheridans.email>
2024-07-21 16:21:10 -0400

    fixed customer data to have all troops in order

--- src/pages/customerData.js
+++ src/pages/customerData.js
@@ -1,15 +1,107 @@
 const CustomerData = [{
   id: 1,
+  name: 'Standard units',
+  image: require("./troop_images/standard_units.PNG"),
+},{
+  id: 2,
+  name: 'Specialized Units',
+  image: require("./troop_images/specialized_units.PNG"),
+},{
+  id: 3,
+  name: 'Big Guns',
+  image: require("./troop_images/big_guns.PNG"),
+},{
+  id: 4,
+  name: 'Combat Engineers',
+  image: require("./troop_images/combat_engineers.PNG"),
+},{
+  id: 5,
+  name: 'Trains',
+  image: require("./troop_images/trains.PNG"),
+},{
+  id: 6,
+  name: 'Supply Trains',
+  image: require("./troop_images/supply_train.PNG"),
+},{
+  id: 7,
+  name: 'Armored Trains',
+  image: require("./troop_images/armoured_trains.PNG"),
+},{
+  id: 8,
+  name: 'Cavalry',
+  image: require("./troop_images/cavalry.PNG"),
+},{
+  id: 9,
+  name: 'Ski Troops',
+  image: require("./troop_images/ski_troops.PNG"),
+},{
+  id: 10,
+  name: 'Snipers',
+  image: require("./troop_images/snipers.PNG"),
+},{
+  id: 11,
+  name: 'Aircraft Carriers',
+  image: require("./troop_images/carriers.PNG"),
+},{
+  id: 12,
+  name: 'Destroyers',
+  image: require("./troop_images/destroyers.PNG"),
+},{
+  id: 13,
+  name: 'Flame Thrower Tanks',
+  image: require("./troop_images/flame_tanks.PNG"),
+},{
+  id: 14,
   name: 'Mobile Artillery',
   image: require("./troop_images/mobile_artillery.PNG"),
 },{
-  id: 2,
-  name: 'Artificial Intelligence',
-  image: require("./troop_images/mobile_artillery.PNG"),
+  id: 15,
+  name: 'Landing Craft',
+  image: require("./troop_images/landing_craft.PNG"),
 },{
-  id: 3,
-  name: 'Intelligence',
-  image: require("./troop_images/mobile_artillery.PNG"),
+  id: 16,
+  name: 'Tigers',
+  image: require("./troop_images/tigers.PNG"),
+},{
+  id: 17,
+  name: 'Supply Trucks',
+  image: require("./troop_images/supply_trucks.PNG"),
+},{
+  id: 18,
+  name: 'Half Tracks',
+  image: require("./troop_images/half_tracks.PNG"),
+},{
+  id: 19,
+  name: 'Long Range Patrol Cars',
+  image: require("./troop_images/patrol_cars.PNG"),
+},{
+  id: 20,
+  name: 'Command Cars',
+  image: require("./troop_images/command_Cars.PNG"),
+},{
+  id: 21,
+  name: 'Brandenburgers',
+  image: require("./troop_images/branded_burgers.PNG"),
+},{
+  id: 22,
+  name: 'Depleted Units',
+  image: require("./troop_images/depleted_units.PNG"),
+},{
+  id: 23,
+  name: 'Heavy Anti Tank Guns',
+  image: require("./troop_images/heavy_at.PNG"),
+},{
+  id: 24,
+  name: 'Tank Destroyers',
+  image: require("./troop_images/tank_destroyers.PNG"),
+},{
+  id: 25,
+  name: 'Screeming Meemies',
+  image: require("./troop_images/screeming_meemies.PNG"),
+},{
+  id: 26,
+  name: 'Hobarts Funniess',
+  image: require("./troop_images/hobarts.PNG"),
 }]
 
   export default CustomerData
