0fe688a404dac47d5d69cb5040109ac77da7dc09
Isaak <isaac@thesheridans.email>
2024-09-01 20:23:17 -0400

    added test filter properties to test my filtering abilities in scenarios.js

---
+++ src/pages/pdfDict.js
@@ -0,0 +1,25 @@
+export const pdfs = [{
+    id: 1,
+    pdf: require('./Saving_Private_Ryan.pdf'),
+    image: require('./saving_private_ryan.PNG'),
+    filter: 'Base Game',
+},
+{
+    id: 2,
+    pdf: require('./Night_drop_in_Holland.pdf'),
+    image: require('./night_drop.PNG'),
+    filter: 'Eastern Front',
+},
+{
+    id: 3,
+    pdf: require('./Across_the_Weser.pdf'),
+    image: require('./across_the_weser.PNG'),
+    filter: 'Mediterranean Theater',
+},
+{
+    id: 4,
+    pdf: require('./Utah_Beach.pdf'),
+    image: require('./utah_beach.PNG'),
+    filter: 'New Flight Plan',
+}
+]
